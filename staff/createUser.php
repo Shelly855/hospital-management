f3e7465fbc495b82416ae6dd02d08c0d5e89c2f7
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-02-01 13:13:43 +0000

    createUser

---
+++ staff/createUser.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/desktop.css" media="only screen and (min-width:720px)" rel="stylesheet" type="text/css">
+    <link href="css/mobile.css" media="only screen and (max-width:720px)" rel="stylesheet" type="text/css">
+    <script src="javascript/main.js" defer></script>
+    <title>Create User</title>
+</head>
+<body>
+    <div class="container">
+        <main>
+            <h1>Create Staff User</h1>
+        </main>
+    </div>
+</body>
+</html>

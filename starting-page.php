173c70ee81049f9412677a5e0e96eeb55937e8d5
Shelly855 <c3011737@hallam.shu.ac.uk>
2024-02-21 14:15:40 +0000

    login

--- starting-page.php
+++ starting-page.php
@@ -10,37 +10,6 @@
 </head>
 <body>
 <div class="container">
-
-  <button onclick="document.getElementById('id02').style.display='block'">Sign Up</button>
-
-  <div id="id02" class="login-form">
-    <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Form">&times;</span>
-    <form class="form-content" action="includes/signup.php" method="POST">
-      <div class="form-box">
-        <h1>Sign Up</h1>
-        <label for="first-name"><b>First Name</b></label>
-        <input type="text" placeholder="Enter First Name" name="first-name" required>
-
-        <label for="surname"><b>Surname</b></label>
-        <input type="text" placeholder="Enter Surname" name="surname" required>
-
-        <label for="email"><b>Email</b></label>
-        <input type="text" placeholder="Enter Email" name="email" required>
-
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
-
-        <label for="psw-repeat"><b>Repeat Password</b></label>
-        <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-          
-        <button type="submit" class="signup">Sign Up</button>
-    </div>
-        <div class="form-box" style="background-color:#f1f1f1">
-          <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
-        </div>
-    </form>
-  </div>
-
   <button onclick="document.getElementById('id01').style.display='block'">Login</button>
   <div id="id01" class="login-form">
     <span onclick="document.getElementById('id01').style.display='none'"
